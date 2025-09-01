f32fb2b752977ab58462a2565e2d77299167ecff
aiyra <emily.aiyra.almeida@gmail.com>
2025-09-01 20:06:52 -0300

    LivroJS - cap04

--- LivroJS/cap02/js/resp2_c.js
+++ LivroJS/cap02/js/resp2_c.js
@@ -6,10 +6,10 @@ frm.addEventListener("submit", (e)=> {
     e.preventDefault()
     const prod = frm.inProd.value
     const preco = Number(frm.inPreco.value)
-
+    const terc = preco/2
     const promo = (2*preco) + (preco/2)
 
     resp1.innerText = `Promoção de ${prod}`
-    resp2.innerText = `Leve 2 por apenas R$: ${promo}`
+    resp2.innerText = `Leve 2 por apenas R$: ${promo.toFixed(2)} \nO 3º sai por R$: ${terc}`
  
 })
---
+++ LivroJS/cap04/js/ex4_1.js
@@ -0,0 +1,24 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
+frm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const nome = frm.inNome.value
+    const nota1 = Number(frm.inNota1.value)
+    const nota2 = Number(frm.inNota2.value)
+    const media = (nota1+nota2)/2
+
+    resp1.innerText = `Média das notas ${media.toFixed(2)}`
+
+    if(media >=7){
+        //mensagem de aprovado e na cor azul
+        resp2.innerText = `Parabéns ${nome}! Aprovado(a).`
+        resp2.style.color = 'blue'
+    } else {
+        //mensagem de reprovado e na cor vermelho
+        resp2.innerText = `Ops! ${nome}... Você foi Reprovado(a).`
+        resp2.style.color = 'red'
+    }
+
+})
