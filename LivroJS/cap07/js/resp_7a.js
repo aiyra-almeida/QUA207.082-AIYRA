b69abc29e6f8f765b2f7cf4eea97acee033d889d
aiyra <emily.aiyra.almeida@gmail.com>
2025-09-26 21:51:47 -0300

    resp_7b trabalhando

--- LivroJS/cap07/js/resp_7a.js
+++ LivroJS/cap07/js/resp_7a.js
@@ -1,12 +1,27 @@
+// O programa deve ler uma mensagem e, em seguida, exibi-la criptografada. A criptografia consiste em: 
+// a) exibir todas as letras das posições pares da mensagem; b) exibir todas as letras das
+// posições ímpares da mensagem.
+
 const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 
-frm.addEventListener("submit", (e)=>{
+frm.addEventListener("submit", (e)=> {
     e.preventDefault()
     const msg = frm.inMsg.value.trim()
-    let cript = ""
-    for (i = 0; i < msg-1; i++){
-        cript = msg.substr(i-1)
-        resp.innerText = `${cript}`
+    let cript1 = ""
+    let cript2 = ""
+    for (let i = 0; i < msg.length; i++){
+        if (i%2 === 0){
+            cript2 += msg[i];
+        }else{
+        cript1 += msg[i];
+        }
     }
+    resp.innerText = cript1 + cript2
+})
+
+frm.btDekript.addEventListener("click", ()=> {
+    const msg = frm.inMsg.value.trim()
+    resp.innerText = `${msg}`
+
 })
---
+++ LivroJS/cap07/js/resp_7b.js
@@ -0,0 +1,19 @@
+// Elaborar um programa que leia
+// uma frase e informe se ela é ou não um palíndromo (converter a frase para
+// caixa alta).
+
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e)=> {
+    e.preventDefault()
+    const frase = frm.inFrase.value.replace(/ /g,"")
+    let palindromo = ""
+    for (let i = frase.lenght-1; i >= 0; i--)
+        palindromo += frase[i]
+    if (frase == palindromo){
+    resp.innerText = `É palíndromo`
+    }else{
+    resp.innerText = `Não é palíndromo`
+    }
+})
