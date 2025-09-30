74a9cda4d0f94c7f75d9b6693ff27a10f7bb80c6
aiyra <emily.aiyra.almeida@gmail.com>
2025-09-30 20:07:16 -0300

    Exemplo de function com return e utilizar retrono em um alert()

--- LivroJS/cap07/js/resp_7c.js
+++ LivroJS/cap07/js/resp_7c.js
@@ -9,12 +9,12 @@ frm.addEventListener("submit", (e)=> {
     const dtDesconto = new Date()
 
     dtDesconto.setFullYear(Number(dtInfracao[0]))
-    dtDesconto.setMonth(Number(dtInfracao[1]+1))
+    dtDesconto.setMonth(Number(dtInfracao[1])-1)
     dtDesconto.setDate(Number(dtInfracao[2])+90)
     // dtDesconto = dtDesconto + (100*60*60*24*90)
     resp1.innerText = "Data Limite para pagamento com Desconto: "
-    resp1.innerText += dtDesconto.getDate().toString.padStar(2,"0") + "/"
-    resp1.innerText += (dtDesconto.getMonth()+1).toString.padStar(2,"0") + "/"
-    resp1.innerText += dtDesconto.getFullYear
+    resp1.innerText += dtDesconto.getDate().toString().padStart(2,"0")+"/"
+    resp1.innerText += (dtDesconto.getMonth()+1).toString().padStart(2,"0")+"/"
+    resp1.innerText += dtDesconto.getFullYear()
     resp2.innerText += "Valor com desconto R$" + (valorMulta*0.8).toFixed(2)
 })
