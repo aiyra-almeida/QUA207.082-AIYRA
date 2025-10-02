b1cfbbb40f16f1a3791793207ccc0a2c48185185
aiyra <emily.aiyra.almeida@gmail.com>
2025-10-02 20:24:37 -0300

    Calculadora funcionando

---
+++ LivroJS/Exemplos/calculadora/js/script.js
@@ -0,0 +1,23 @@
+// função para adicionar um valor
+function adicionar(valor){
+    document.querySelector("#visor").value += valor
+}
+// função para limpar o vistor
+function limpar(){
+    document.querySelector("#visor").value = ""
+}
+// função para apagar o ultimo caracter
+function apagar(){
+   const visor = document.querySelector("#visor")
+   visor.value = visor.value.slice(0,-1)
+} 
+// função para calcular resultado
+function calcular(){
+    const visor = document.querySelector("#visor")
+    try {
+        // usar a function eval() para calcular o resultado
+        visor.value = eval(visor.value)
+    } catch (error) {
+        visor.value = "Error"
+    }
+}
