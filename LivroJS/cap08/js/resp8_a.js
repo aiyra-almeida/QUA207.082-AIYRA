625e204cc8b39ee76234d8636ce86b83f85b9f0a
aiyra <emily.aiyra.almeida@gmail.com>
2025-10-02 21:46:20 -0300

    Respostas Exercício 8 - Resp8_a

---
+++ LivroJS/cap08/js/resp8_a.js
@@ -0,0 +1,12 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    const nome = frm.inNome.value
+    const idade = frm.inIdade.value
+    function retornarTracos(){
+    
+    }
+})
