ee65245615967023847bfab0e6d9677571429c55
aiyra <emily.aiyra.almeida@gmail.com>
2025-10-03 20:15:47 -0300

    resp8_a finalizada +/-

--- LivroJS/cap08/js/resp8_a.js
+++ LivroJS/cap08/js/resp8_a.js
@@ -1,12 +1,26 @@
 const frm = document.querySelector("form")
 const resp1 = document.querySelector("#outResp1")
 const resp2 = document.querySelector("#outResp2")
+const resp3 = document.querySelector("#outResp3")
 
-frm.addEventListener("submit", (e) =>{
+frm.addEventListener("submit", (e) => {
     e.preventDefault()
     const nome = frm.inNome.value
-    const idade = frm.inIdade.value
-    function retornarTracos(){
-    
+    const idade = Number(frm.inIdade.value)
+    function retornarTracos() {
+        return nome.replace(/[^ ]/g,"-")
     }
+    
+    function categorizarAluno() {
+        if (idade <= 12) {
+            return "Infantil"
+        } else if (idade <= 18) {
+            return "Juvenil"
+        } else {
+            return "Adulto"
+        }
+    }
+    resp1.innerText = `${nome}`
+    resp2.innerText = retornarTracos()
+    resp3.innerText = categorizarAluno()
 })
