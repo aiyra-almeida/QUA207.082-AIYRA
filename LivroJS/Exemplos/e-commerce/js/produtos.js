8d06674bf7af8c553befbfc0d2c4d63af76d2800
aiyra <emily.aiyra.almeida@gmail.com>
2025-10-22 21:47:33 -0300

    projeto e-commerce - atualização de produtos

---
+++ LivroJS/Exemplos/e-commerce/js/produtos.js
@@ -0,0 +1,38 @@
+const lsProdutos = [
+    {
+        nome: "Banho de Mar",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-01.png",
+        valor: 39
+    },
+    {
+        nome: "Passeio na Floresta",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-02.png",
+        valor: 39
+    },
+    {
+        nome: "Meus Cristais",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-03.png",
+        valor: 39
+    },
+    {
+        nome: "Vida no Campo",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-04.png",
+        valor: 39
+    },
+    {
+        nome: "Meu Momento",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-05.png",
+        valor: 39
+    },
+    {
+        nome: "Floresta Viva",
+        descricao: "vela aromática - 300g",
+        img: "img/vela-04.png",
+        valor: 39
+    }
+]
